249ab2278135a971fc078d9fecb94a3a56159f50
atarannn <anastasiatarannn@icloud.com>
2023-05-03 11:15:01 +0300

    sport mobile layout

---
+++ src/assets/scripts/gulp-modules/sport-mobile-slider-2.js
@@ -0,0 +1,10 @@
+const slider3 = new Swiper('.section-2-mobile-slider', {
+  loop: false,
+  speed: 1000,
+  slidesPerView: 1,
+  spaceBetween: 20,
+  pagination: {
+    el: '.swiper-pagination-mobile',
+    clickable: true,
+  },
+});
