13d2863a872bb91ad81f0d8ff63b7a83522b4e1b
Dron2019 <dron_2019@ukr.net>
2023-05-03 11:40:11 +0300

    location page adaptation

--- src/assets/scripts/gulp-modules/location-slider-1.js
+++ src/assets/scripts/gulp-modules/location-slider-1.js
@@ -22,3 +22,55 @@ const slider = new Swiper('.location-section-5-slider', {
   // },
   watchSlidesVisibility: true,
 });
+
+
+
+window.addEventListener('DOMContentLoaded', () => {
+  if (!document.documentElement.classList.contains('mobile')) return;
+
+  const swiper = new Swiper('[data-location-section-2-mobile-slider]', {
+    loop: false,
+    speed: 1000,
+    slidesPerView: 1.6,
+    spaceBetween: 20,
+    pagination: {
+      el: '.swiper-pagination-mobile',
+      clickable: true,
+    },
+  });
+  window.addEventListener('DOMContentReloaded', () => {
+    swiper.destroy();
+  })
+});
+
+/**Section 7 mobile slider */
+window.addEventListener('DOMContentLoaded', () => {
+  if (!document.documentElement.classList.contains('mobile')) return;
+
+  const wrapper = document.querySelector('.gallery-wrapper');
+
+  wrapper.innerHTML = `
+    <div class="swiper-wrapper">
+      ${Array.from(wrapper.children).map(el => {
+
+        el.classList.add('swiper-slide');
+        return el.outerHTML;
+      }).join('')}
+    </div>
+    <div class="swiper-pagination-mobile"></div>
+  `;
+
+  const swiper = new Swiper(wrapper, {
+    loop: false,
+    speed: 1000,
+    // slidesPerView: 1.6,
+    // spaceBetween: 20,
+    pagination: {
+      el: '.gallery-wrapper .swiper-pagination-mobile',
+      clickable: true,
+    },
+  });
+  // window.addEventListener('DOMContentReloaded', () => {
+  //   swiper.destroy();
+  // })
+})
