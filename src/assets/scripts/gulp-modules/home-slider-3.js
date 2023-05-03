c1930cb476a224a79cbed0908e4fff2a637c0b9d
yatsinoa <sashayatzino@gmail.com>
2023-05-03 03:01:55 +0300

    children page, homepage slider

--- src/assets/scripts/gulp-modules/home-slider-3.js
+++ src/assets/scripts/gulp-modules/home-slider-3.js
@@ -1,7 +1,8 @@
 const slider3 = new Swiper('.section-2-mobile-slider', {
   loop: false,
   speed: 1000,
-  slidesPerView: 1.2,
+  slidesPerView: 1.6,
+  spaceBetween: 20,
   pagination: {
     el: '.swiper-pagination-mobile',
     clickable: true,
